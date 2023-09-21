60b38d194da3bf93dfe7549fbbe9a7223939d251
Danil Kabanov <nill2@yandex.ru>
2023-09-21 21:31:32 +0200

    feat: min structure

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/profile')
+def profile():
+    return render_template('profile.html')
