109c2a69d205e77997717f558a4ede842564e462
Danil Kabanov <nill2@yandex.ru>
2024-02-19 02:13:29 +0100

    feat: cctv + cleanup (#4)

--- auth.py
+++ auth.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for,Blueprint
+'''
+Process the auth requests
+'''
+from flask import render_template, Blueprint
+
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/signup', methods=['GET', 'POST'])
-def signup():
-    return render_template('signup.html')
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    '''
+    a stub for login
+    '''
     return render_template('login.html')
+
+
 @auth.route('/logout', methods=['GET', 'POST'])
 def logout():
-    return 'this is a stub for a logout'##redirect(url_for('auth.login'))
+    '''
+    a stub for logout
+    '''
+    return 'this is a stub for a logout'  # redirect(url_for('auth.login'))
